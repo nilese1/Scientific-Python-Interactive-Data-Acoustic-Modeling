baa9854b3713901c39b28c1b1e5cf6c8a544d2c3
Ethan Niles <notethansemail123456789@gmail.com>
2023-11-21 16:42:22 -0500

    Added loadfile modue and updated requirements

---
+++ controller/loadfile.py
@@ -0,0 +1,43 @@
+from tkinter import filedialog
+import os
+from pydub import AudioSegment 
+
+
+def convert_to_wav(file):
+    # Import audio file
+    try:
+        audio = AudioSegment.from_file(file.name)
+
+    except:
+        os.error('Something has gone horribly wrong (maybe not an audio file?)')
+  
+    # Create new filename
+    new_filename = file.name.split('.')[0] + '.wav'
+  
+    # Export file as .wav
+    audio.export(new_filename, format='wav')
+     
+    file.close()
+    new_file = open(new_filename, 'r')
+
+    return new_file
+
+
+def prompt_audio_file():
+    try:
+        temp = filedialog.askopenfile(mode='r')
+    
+    except FileNotFoundError:
+        os.error('Incorrect File Given')
+    
+    filename = temp.name
+    print(filename)
+    is_wav = filename.endswith('.wav')
+
+    if not is_wav:
+        temp = convert_to_wav(temp)
+    
+    return temp
+
+
+prompt_audio_file()
