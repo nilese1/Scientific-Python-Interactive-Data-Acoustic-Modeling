83f43ad17388b6b533a8a9598c5467f706f0e805
connor davis <davisconnorgc2003@gmail.com>
2023-12-09 19:20:47 -0500

    Began basis in plotRT60.py for functions to calculate and plot frequencies.

--- plotRT60.py
+++ plotRT60.py
@@ -1,3 +1,22 @@
 # This module will hand the functions necessary for displaying the graphs of the
 # RT60 data for Low, mid and high frequencies. These functions will be used in the spidam-gui.py
 # module to display the charts at press of a button.
+
+def plotHigh():
+    pass
+
+def plotMid():
+    pass
+
+def plotLow():
+    pass
+
+def calcHigh():
+    pass
+
+def calcMid():
+    pass
+
+def calcLow():
+    pass
+
