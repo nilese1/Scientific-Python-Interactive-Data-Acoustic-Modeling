035ba2728a6b3b524cc782851bbce46bf3c42d91
connor davis <davisconnorgc2003@gmail.com>
2023-12-09 19:11:05 -0500

    minor order changes in gui, new module, plotRT60.py for calculating and plotting data

---
+++ plotRT60.py
@@ -0,0 +1,3 @@
+# This module will hand the functions necessary for displaying the graphs of the
+# RT60 data for Low, mid and high frequencies. These functions will be used in the spidam-gui.py
+# module to display the charts at press of a button.
